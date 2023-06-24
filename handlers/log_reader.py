e8655dfbb565882884975b4eeb5855c7e27f9359
kristijankoscak <kristijan.koscak16@gmail.com>
2023-06-24 12:54:07 +0200

    Implement log_reader

---
+++ handlers/log_reader.py
@@ -0,0 +1,46 @@
+from os import walk, remove
+
+LOG_TYPE_ADMIN = 'admin_'
+LOG_TYPE_LOGIN = 'login_'
+
+class LogReader:
+    path_to_logs='./logs'
+    admin_file_position = 1
+
+    def __init__(self) -> None:
+        pass
+
+    def remove_old_logs(self, log_type, current_log_filename):
+        for (dirpath, dirnames, filenames) in walk(self.path_to_logs):
+            for filename in filenames:
+                if log_type in filename and current_log_filename != filename:
+                    remove('%s/%s'%(self.path_to_logs, filename))
+                    self.set_file_position_by_type(log_type=log_type, new_value=0)
+
+    def set_file_position_by_type(self, log_type, new_value):
+        if log_type == LOG_TYPE_ADMIN:
+            self.admin_file_position = new_value
+
+    def get_file_position_by_type(self, log_type):
+        if log_type == LOG_TYPE_ADMIN:
+            return self.admin_file_position
+        
+    def get_messages_by_type(self, log_type, current_log_filename) -> list[str]:
+        file_position = self.get_file_position_by_type(log_type=log_type)
+        file = open('%s/%s'%(self.path_to_logs, current_log_filename), "r")
+        messages = []
+
+        for index, line in enumerate(file):
+            if index > file_position:
+                messages.append(line)
+                file_position = index
+        
+        self.set_file_position_by_type(log_type=log_type, new_value=file_position)
+
+        return messages
+
+
+    def get_messages_for_discord_by_type(self, log_type, current_log_filename) -> list[str]:
+        self.remove_old_logs(log_type=log_type, current_log_filename=current_log_filename)
+
+        return self.get_messages_by_type(log_type=log_type, current_log_filename=current_log_filename)
