8b89f33efb2a696be11ea27568255a0abf9871c2
kristijankoscak <kristijan.koscak16@gmail.com>
2023-06-24 12:54:29 +0200

    Implement discord handler

---
+++ handlers/discord.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from handlers.log_reader import LOG_TYPE_ADMIN
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+class DiscordHandler:
+    def __init__(self, webhook_url) -> None:
+        self.webhook_url = webhook_url        
+
+
+    def extract_data_from_admin_log(self, line): 
+        line_parts = line.split('\'', 2)
+
+        date_time = line_parts[0].replace(':', '')
+        user = line_parts[1].replace('\'', '')
+        command = line_parts[2].replace('\'', '')
+
+        return date_time, user, command
+
+    def send_message(self, embed):
+        webhook = DiscordWebhook(url=self.webhook_url)
+        
+        webhook.add_embed(embed=embed)
+        webhook.execute()
+
+    
+    def send_admin_message(self, message):
+        today = datetime.today().strftime('%Y-%m-%d %H:%m')
+        _, user, command = self.extract_data_from_admin_log(message)
+        admin_embed = DiscordEmbed(
+            title=user, 
+            description=command,
+            author=dict(
+                name= "🅰dministrator"
+            ),
+            color="FF0000",
+            timestamp=today)
+        
+        print(user)
+        print(command)
+        self.send_message(embed=admin_embed)
+
+    def send_messages_by_type(self, log_type, messages):
+        for message in messages:
+            if log_type == LOG_TYPE_ADMIN:
+                self.send_admin_message(message=message)
+
